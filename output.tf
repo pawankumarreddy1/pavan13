33b22b4df381f8e33b7ce7572e7a14fdaf3a98cb
pavankumarreddy <pavankumareddy635@gmail.com>
2022-09-14 12:45:34 +0530

    first commit

---
+++ output.tf
@@ -0,0 +1,30 @@
+output "zones" {
+  value = data.aws_availability_zones.available.names
+}
+
+output "vpcname" {
+  value = aws_vpc.stag-vpc.id
+}
+
+output "countofaz" {
+  value = length(data.aws_availability_zones.available.names)
+}
+output "cicdip" {
+  value = aws_instance.jenkins2.public_ip
+
+}
+
+output "apache2ip" {
+  value = aws_instance.apache2.public_ip
+
+}
+
+output "apache2pubdns" {
+  value = aws_instance.apache2.public_dns
+
+}
+
+output "cicdpubdns" {
+  value = aws_instance.jenkins2.public_dns
+
+}
